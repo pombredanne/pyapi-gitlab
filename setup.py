d3855a6f61463e9e4dc2ab84983771912b188875
Itxaka Serrano <iserrano@e-commfactory.com>
2013-10-07 15:56:00 +0200

    fixed the package name

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup, find_packages
 setup(
-    name = "python-gitlab",
+    name = "i-python-gitlab",
     version = "5.4",
     packages = find_packages(),
     install_requires = ['requests', 'markdown'],
