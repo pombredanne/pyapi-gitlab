7d8f28cc13f9a17d98d206684df6d7eb32ed962a
Itxaka Serrano <iserrano@e-commfactory.com>
2013-10-07 15:54:06 +0200

    updated version number

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@
 from setuptools import setup, find_packages
 setup(
     name = "python-gitlab",
-    version = "0.1",
+    version = "5.4",
     packages = find_packages(),
     install_requires = ['requests', 'markdown'],
     # metadata for upload to PyPI
     author = "Itxaka Serrano Garcia",
     author_email = "itxakaserrano@gmail.com",
-    description = "See the README.md file for more information",
+    description = "Python gitlab wrapper, for use with gitlab 5.x version",
     license = "GPL3",
     keywords = "gitlab git wrapper",
     url = "http://github.com/itxaka/python-gitlab/",
