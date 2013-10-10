04fa25c89ac33dd47ea23018cc934e47bce24ab2
itxaka <iserrano@e-commfactory.com>
2013-10-10 16:55:05 +0200

    changed name to pyapi-gitlab, final name I promise

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     # metadata for upload to PyPI
     author = "Itxaka Serrano Garcia",
     author_email = "itxakaserrano@gmail.com",
-    description = "Python gitlab wrapper, for use with gitlab 5.x version",
+    description = "Gitlab API wrapper for Gitlab 5.X",
     license = "GPL3",
     keywords = "gitlab git wrapper",
     url = "http://github.com/itxaka/pyapi-gitlab/",
